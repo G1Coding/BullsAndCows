ba267658da7074e6117e488aff6ce81976889067
yeongho3358 <youngho3358@gmail.com>
2024-03-11 18:21:34 +0900

    first commit(test)

---
+++ src/main/java/main/ChildClass.java
@@ -0,0 +1,10 @@
+package main;
+
+public class ChildClass {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("z");
+
+        }
+    }
+}
---
+++ src/main/java/main/MainClass.java
@@ -0,0 +1,9 @@
+package main;
+
+public class MainClass {
+    public static void main(String[] args) {
+        while(true){
+            System.out.println("a");
+        }
+    }
+}
